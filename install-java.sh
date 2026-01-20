4dff840d6f72f83d4fc6179ca0beed14bb81d1cf
d036667 <kay.wegner@sap.com>
2026-01-20 08:01:31 +0100

    test: connectivity additional command

--- install-java.sh
+++ install-java.sh
@@ -8,3 +8,7 @@ tar -xzf sapmachine-jdk.tar.gz
 export JAVA_HOME="$(pwd)/${JAVA_WORKDIR}"
 export PATH="${JAVA_HOME}/bin:${PATH}"
 java -version
+
+JAVA_PRIVATE_URL="http://myPrivateRepo:8080/SAP/SapMachine/releases/download/sapmachine-${JAVA_VERSION}/${JAVA_WORKDIR}_linux-x64_bin.tar.gz"
+wget -q -e use_proxy=yes -e http_proxy="${connectivity_proxy}" "${JAVA_PRIVATE_URL}" -O sapmachine-from-private-repo.tar.gz
+ls -ltr
